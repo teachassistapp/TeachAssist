969e00ad05e2a11efa1780064761d61343f3ef72
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2022-08-20 20:22:15 -0400

    update colours

--- globals/colors.js
+++ globals/colors.js
@@ -1,15 +1,15 @@
 export const lightColors = {
-  Primary1: "#69A863",
-  Primary2: "#70C269",
-  Final: "#63d959",
-  Other: "#5e7d5b",
-  Background: "#FAFAFA", // bg colour
+  Primary1: "#60AA5A",
+  Primary2: "#7BE372",
+  Final: "#6efa61",
+  Other: "#6efa61",
+  Background: "#FEFEFE", // bg colour
   Header: "#2f2f2f", // main text
-  Subtitle: "#888888", // sub text AND unselected bottom navigation icons
+  Subtitle: "#777", // sub text AND unselected bottom navigation icons
   Placeholder: "#CECECE", // placeholder for log in text boxes / search bar
-  Container: "#FDFDFD", // bg colour for boxes that are everywhere
+  Container: "#f8f8f8", // bg colour for boxes that are everywhere
   Border: "#EEEEEE", // border for boxes that are everywhere
-  Selected: "#fdfdfd", // for sliders (i.e. assignments/statistics/details)
+  Selected: "#FEFEFE", // for sliders (i.e. assignments/statistics/details)
   GraphBackground: "#EEEEEE", // background for circle / bar graphs
   Shadow: "#ddd",
   Red: "#FF3B30",
@@ -21,17 +21,17 @@ export const darkColors = {
   Primary1: "#60AA5A",
   Primary2: "#7BE372",
   Final: "#6efa61",
-  Other: "#678c64",
-  Background: "#161818",
+  Other: "#6efa61",
+  Background: "#0B0D0E",
   Header: "#fafafa",
   Subtitle: "#D3D3D3",
-  Placeholder: "#333333",
-  Container: "#202323",
-  Border: "#2f3030",
-  Selected: "#2a2c2c",
-  GraphBackground: "#333333",
-  Shadow: "#202323",
+  Placeholder: "#27292A",
+  Container: "#151617",
+  Border: "#202223",
+  Selected: "#1C1E1F",
+  GraphBackground: "#27292A",
+  Shadow: "#151617",
   Red: "#FF3B30",
-  Bone: "#2a2c2c",
-  SwitchBg: "#202323"
+  Bone: "#1C1E1F",
+  SwitchBg: "#151617"
 };
